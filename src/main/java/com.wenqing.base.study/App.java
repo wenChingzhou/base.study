b167379eb9d175a496f33273fb4b06f4c04e7b0b
wenChingzhou <65765967+wenChingzhou@users.noreply.github.com>
2022-11-28 10:45:58 +0800

    Add files via upload

---
+++ src/main/java/com.wenqing.base.study/App.java
@@ -0,0 +1,13 @@
+package com.wenqing.study.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com.wenqing.base.study/net/TEST.java
@@ -0,0 +1,8 @@
+package com.wenqing.base.study.net;
+
+/**
+ * @author xiaowei.zhou
+ * @date 2022/11/18
+ */
+public class TEST {
+}
